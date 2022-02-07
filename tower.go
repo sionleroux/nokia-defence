3248144b3ab7b4017f040aff8ec4b52ea933713a
Siôn le Roux <sinisterstuf@gmail.com>
2022-02-07 01:35:10 +0100

    Add constructors for different tower types
    The best thing about this is that it does away with the init function
    and its associated globals.  If I wanted to spend time on it I could now
    test these.

--- tower.go
+++ tower.go
@@ -10,41 +10,43 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-var (
-	ImageBasicTower  *ebiten.Image
-	ImageStrongTower *ebiten.Image
-)
-
-func init() {
-
-	i := image.NewPaletted(
-		image.Rect(0, 0, 5, 5),
-		NokiaPalette,
-	)
-
-	i.Pix = []uint8{
-		2, 2, 2, 2, 2,
-		2, 1, 1, 1, 2,
-		2, 1, 1, 1, 2,
-		2, 1, 1, 1, 2,
-		2, 2, 2, 2, 2,
-	}
-	ImageBasicTower = ebiten.NewImageFromImage(i)
-
-	i.Pix = []uint8{
-		2, 2, 2, 2, 2,
-		2, 2, 1, 2, 2,
-		2, 1, 1, 1, 2,
-		2, 2, 1, 2, 2,
-		2, 2, 2, 2, 2,
-	}
-	ImageStrongTower = ebiten.NewImageFromImage(i)
-
-}
-
 // Tower can be placed at a position to shoot Creeps
 type Tower struct {
 	Coords image.Point
+	Image  *ebiten.Image
+}
+
+// NewTower makes a new tower provided image pixel input
+func NewTower(coords image.Point, size int, pix []uint8) *Tower {
+	i := image.NewPaletted(
+		image.Rect(0, 0, size, size),
+		NokiaPalette,
+	)
+	i.Pix = pix
+
+	return &Tower{coords, ebiten.NewImageFromImage(i)}
+}
+
+// NewBasicTower is a convenience wrapper to make a basic-looking tower
+func NewBasicTower(coords image.Point) *Tower {
+	return NewTower(coords, 5, []uint8{
+		2, 2, 2, 2, 2,
+		2, 1, 1, 1, 2,
+		2, 1, 1, 1, 2,
+		2, 1, 1, 1, 2,
+		2, 2, 2, 2, 2,
+	})
+}
+
+// NewStrongTower is a convenience wrapper to make a strong-looking tower
+func NewStrongTower(coords image.Point) *Tower {
+	return NewTower(coords, 5, []uint8{
+		2, 2, 2, 2, 2,
+		2, 2, 1, 2, 2,
+		2, 1, 1, 1, 2,
+		2, 2, 1, 2, 2,
+		2, 2, 2, 2, 2,
+	})
 }
 
 // Update handles game logic for towers
@@ -56,7 +58,7 @@ func (t *Tower) Update(g *Game) {
 func (t *Tower) Draw(g *Game, screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(t.Coords.X-1), float64(t.Coords.Y-1))
-	screen.DrawImage(ImageBasicTower, op)
+	screen.DrawImage(t.Image, op)
 }
 
 // Towers is a slice of Tower entities
