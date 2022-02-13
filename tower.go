9413b53d968d172d7db50b5638841c1da93937b3
Siôn le Roux <sinisterstuf@gmail.com>
2022-02-13 07:47:23 +0100

    Draw lasers shooting creeps within 1 tile range of a tower
    Such a good feeling that this could be implemented by only editing the
    towers.go file :) I added a new Target field to the tower to track which
    creep it's attacking.  If a creep's centre point is within the range of
    1 7x7 tile in any direction of the tower (including diagonally) then it
    sets it as the target.  A line will be drawn from the tower to the
    target to represent a laser beam.  Because the creeps currently have no
    concept of health the laser has nothing to damage so the creep just
    keeps walking but it has a really cool effect as the towers' beams
    follow the creeps along the map.

--- tower.go
+++ tower.go
@@ -9,6 +9,7 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 // Tower can be placed at a position to shoot Creeps
@@ -16,6 +17,7 @@ type Tower struct {
 	Coords image.Point
 	Cost   int
 	Frame  int
+	Target int // the creep it's currently attacking
 	Sprite *SpriteSheet
 }
 
@@ -25,7 +27,7 @@ func NewBasicTower(g *Game) *Tower {
 	if !ok {
 		log.Fatal("Failed to retrieve basic tower from game resource map")
 	}
-	return &Tower{g.Cursor.Coords, 200, 0, sprite}
+	return &Tower{g.Cursor.Coords, 200, 0, -1, sprite}
 }
 
 // NewStrongTower is a convenience wrapper to make a strong-looking tower
@@ -35,18 +37,39 @@ func NewStrongTower(g *Game) *Tower {
 	if !ok {
 		log.Fatal("Failed to retrieve strong tower from game resource map")
 	}
-	return &Tower{g.Cursor.Coords, 500, 0, sprite}
+	return &Tower{g.Cursor.Coords, 500, 0, -1, sprite}
 }
 
 // Update handles game logic for towers
 func (t *Tower) Update(g *Game) {
+	// Construction animation
 	if t.Frame < len(t.Sprite.Sprite)-1 {
 		t.Frame++
+	}
+
+	// Look for the first creep in range
+	tileSize := 7
+	for k, v := range g.Creeps {
+		withinRange := image.Rectangle{
+			v.(*Creep).Coords,
+			v.(*Creep).Coords,
+		}.In(
+			image.Rect(
+				t.Coords.X-tileSize,
+				t.Coords.Y-tileSize,
+				t.Coords.X+tileSize,
+				t.Coords.Y+tileSize,
+			))
+		if withinRange {
+			t.Target = k
+		}
 	}
 }
 
 // Draw draws the Tower to the screen
 func (t *Tower) Draw(g *Game, screen *ebiten.Image) {
+
+	// Draw tower
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(t.Coords.X-1), float64(t.Coords.Y-1))
 	s := t.Sprite
@@ -57,6 +80,18 @@ func (t *Tower) Draw(g *Game, screen *ebiten.Image) {
 		frame.Position.X+frame.Position.W,
 		frame.Position.Y+frame.Position.H,
 	)).(*ebiten.Image), op)
+
+	// Draw shooting laser
+	if t.Target != -1 {
+		c := g.Creeps[t.Target].(*Creep)
+		ebitenutil.DrawLine(screen,
+			float64(t.Coords.X),
+			float64(t.Coords.Y),
+			float64(c.Coords.X),
+			float64(c.Coords.Y),
+			ColorDark,
+		)
+	}
 }
 
 // Towers is a slice of Tower entities
